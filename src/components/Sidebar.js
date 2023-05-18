86228c7b952e8da125d0d28d53cdb12c340ba561
brajpatel <brajeshpatel0002@gmail.com>
2023-05-18 11:19:48 +0100

    Show characters to find with corresponding image

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,24 @@
 import React from "react";
 import Timer from "./Timer";
 
-function Sidebar() {
+function Sidebar(props) {
+    const { charData } = props;
+
     return (
         <div className="sidebar">
             <div>
                 <Timer/>
-                <div className="character-list">
+                <div className="character-list-container">
                     <h2>Characters to find:</h2>
-                    <ul>
-
+                    <ul className="character-list">
+                        {charData.map((char) => {
+                            return (
+                                <li key={char.id}>
+                                    <p>{char.name}<i class="fa-solid fa-image"></i></p>
+                                    <img src={char.image}  alt={char.name}/>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </div>
             </div>
