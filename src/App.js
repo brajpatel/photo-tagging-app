515b2709c26f3c70d1a484f5368ea032411b2207
brajpatel <brajeshpatel0002@gmail.com>
2023-05-18 10:30:31 +0100

    Create basic app structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import './App.css';
 import React from 'react';
-import { useRef } from 'react';
-import handleSubmit from './components/Test';
 
 function App() {
-  const dataRef = useRef()
-
-  const submithandler = (e) => {
-    e.preventDefault()
-    handleSubmit(dataRef.current.value)
-    dataRef.current.value = "";
-  }
-
   return (
-    <div className="App">
-      <form onSubmit={submithandler}>
-        <input type="text" ref={dataRef}/>
-        <button type="submit">Save</button>
-      </form>
+    <div className="app">
+      Is it on?
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h2>Characters to find:</h2>
+        </div>
+    )
+}
+
+export default Sidebar;
