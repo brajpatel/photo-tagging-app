0e6ffd17eac7083f4ab1c8438ca9c36e49b2e7e8
brajpatel <brajeshpatel0002@gmail.com>
2023-05-18 11:09:21 +0100

    Create array of character data

--- src/App.js
+++ src/App.js
@@ -2,11 +2,44 @@ import './App.css';
 import React from 'react';
 import Sidebar from './components/Sidebar';
 import Game from './components/Game';
+import diane from "./assets/diane.png";
+import patrick from "./assets/patrick.png";
+import link from "./assets/link.png";
+import noFace from "./assets/no-face.png";
+import aang from "./assets/aang.png";
+
+const charData = [
+  {
+    name: 'Diane',
+    id: 'diane',
+    image: diane
+  },
+  {
+    name: 'Patrick',
+    id: 'patrick',
+    image: patrick
+  },
+  {
+    name: 'Link',
+    id: 'link',
+    image: link
+  },
+  {
+    name: 'No Face',
+    id: 'no-face',
+    image: noFace
+  },
+  {
+    name: 'Aang',
+    id: 'aang',
+    image: aang
+  },
+]
 
 function App() {
   return (
     <div className="app">
-      <Sidebar/>
+      <Sidebar charData={charData}/>
       <Game/>
     </div>
   );
