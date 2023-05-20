85b13bda8910e2e5232437688ddfa8792461bbfc
brajpatel <brajeshpatel0002@gmail.com>
2023-05-20 15:18:10 +0100

    Style game start message elements

--- src/components/GameStart.js
+++ src/components/GameStart.js
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
 function GameStart() {
+
+    const [playerName, setPlayerName] = useState('');
+    const [disabled, setDisabled] = useState(true);
+
+    const handleName = (e) => {
+        e.target.value === '' ? setDisabled(true) : setDisabled(false);
+
+        setPlayerName(e.target.value);
+    }
+
     return (
         <div className="overlay">
-            <div>
-
+            <div className="game-message game-start">
+                <h1>Where's Waldo?<i class="fa-solid fa-magnifying-glass"></i></h1>
+                <p>Enter your name below and click play once you're ready.</p>
+                <form>
+                    <input onInput={handleName} placeholder="Enter your name here" type="text" autoFocus/>
+                    <button type="submit" disabled={disabled}>Play Game</button>
+                </form>
             </div>
         </div>
     )
